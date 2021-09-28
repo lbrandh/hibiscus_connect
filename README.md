18174e4aa94d9a1a71a73f167a63961e138e96c2
David Malinowski <dm@itsdave.de>
2021-09-28 20:46:41 +0200

    feat: Initialize App

---
+++ README.md
@@ -0,0 +1,7 @@
+## Hibiscus Connect
+
+Synchronisation zwischen der ERPNext und der Online-Banking Software Hibiscus
+
+#### License
+
+GPLv3
---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+with open("requirements.txt") as f:
+	install_requires = f.read().strip().split("\n")
+
+# get version from __version__ variable in hibiscus_connect/__init__.py
+from hibiscus_connect import __version__ as version
+
+setup(
+	name="hibiscus_connect",
+	version=version,
+	description="Synchronisation zwischen der ERPNext und der Online-Banking Software Hibiscus",
+	author="itsdave GmbH",
+	author_email="dev@itsdave.de",
+	packages=find_packages(),
+	zip_safe=False,
+	include_package_data=True,
+	install_requires=install_requires
+)
