77c4b1b6ac8af3799d17ca206e101de46e8c434c
Beate Trzensiok <b.trzensiok@itsdave.de>
2021-10-22 13:59:49 +0200

    WIP

--- hibiscus_connect/tools.py
+++ hibiscus_connect/tools.py
@@ -2,6 +2,7 @@ import frappe
 from hibiscus_connect.hibclient import Hibiscus
 import json
 from pprint import pprint
+from datetime import datetime as dt
 
 @frappe.whitelist()
 
@@ -26,6 +27,7 @@ def get_accounts_from_hibiscus_server_for_dialog():
         }
         fields.append(account_dict)
     return fields
+
 
 @frappe.whitelist()
 
@@ -52,11 +54,33 @@ def create_hibiscus_connect_bank_account(hib_acc):
 def get_transactions_for_account(id, von, bis):
     settings = frappe.get_single("Hibiscus Connect Settings")
     hib = Hibiscus(settings.server, settings.port, settings.master_password, settings.ignore_cert)
-    transactions = hib.get_transactions(id)
-    pprint(transactions[len(transactions)-1])
-    pprint(transactions[len(transactions)-2])
-    pprint(transactions[len(transactions)-3])
+    
+    von_dt = dt.strptime (von,"%Y-%m-%d")
+    bis_dt = dt.strptime (bis,"%Y-%m-%d")
+    
+    transactions = hib.get_transactions(id, von_dt,bis_dt)
+    check_trans_id = frappe.get_all("Hibiscus Connect Transaction", fields = "id")
+    check_trans_id_list = [ x["id"] for x in check_trans_id]
+    
+    for hib_trans in transactions:
+        if hib_trans["id"] not in check_trans_id_list: 
+            create_hibiscus_connect_transaction(hib_trans)
+        
+    # pprint(transactions[len(transactions)-1])
+    # pprint(transactions[len(transactions)-2])
+    # pprint(transactions[len(transactions)-3])
     print(von)
     print(bis)
+ 
 
+
+def create_hibiscus_connect_transaction(hib_trans):
     
+    hib_trans["doctype"] = "Hibiscus Connect Transaction"
+    hib_trans["saldo"] = float(str(hib_trans["saldo"]))
+    hib_trans["betrag"] = float(str(hib_trans["betrag"]).replace(".","").replace(",","."))
+    hib_trans["zweck_raw"] = hib_trans["zweck"]
+    hib_trans_doc = frappe.get_doc(hib_trans)
+    hib_trans_doc.save()
+
+        
